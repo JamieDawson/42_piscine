701fe671b926236c23dda3cfa919fa96508a0933
Jamie Dawson <jadawson@e1z3r4p21.42.us.org>
2018-07-31 20:10:48 -0700

    added day04

---
+++ day04/ex04/ft_fibonacci.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/28 23:08:32 by jadawson          #+#    #+#             */
+/*   Updated: 2017/10/28 23:21:02 by jadawson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	else if (index <= 1)
+		return (index);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
